690153e44621a021e0ca16c93d84f74e56519ec1
MekadAlIibrahem <mekadalibrahem123@gmail.com>
2024-03-19 23:56:08 +0300

    publich views

--- src/UiAuthPackageServiceProvider.php
+++ src/UiAuthPackageServiceProvider.php
@@ -12,8 +12,16 @@ class UiAuthpackageServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
-        $this->loadViewsFrom(__DIR__.'/resources/views', 'blogpackage');
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'uiauthpackage');
+        $this->publishes([
+            __DIR__.'/../resources/views' => resource_path('views/vendor/uiauthpackage'),
+        ]);
+
+        // $this->publishes([
+        //     __DIR__.'/../routes/web.php' => resource_path('resource/vendor/uiauthpackage'),
+        // ]);
+    
     }
 }
 
