b806a322f75a89b532f5070ec525df82aac08bc2
MekadAlIibrahem <mekadalibrahem123@gmail.com>
2024-03-19 15:31:58 +0300

    init controller and add login controller

---
+++ src/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace MekadAlibrahem\Uiauthpackage\Http\Controllers ;
+
+use Illuminate\Http\Request ;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller 
+{
+    public function login_handler(Request $request){
+        $request()->validate([
+            'username' => "required", 
+            'password' => 'required'
+        ]);
+
+        $credentials = [
+            'username' => $request->get('username') ,
+            'password'=> $request->get('password')
+        ];
+        if(Auth::attempt($credentials)){
+
+            return redirect()->route('user.home');
+        }else{
+            $request->session()->put('login_failer' , 'فشل تسجيل الدخول : كلمة المرور غير صحيحية');
+            return redirect()->route('view_login') ;
+        }
+    }
+}
+
+
+
